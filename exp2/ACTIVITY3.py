e540817ca0f587bcfc2caf3212f1c51739eeaa38
gogawaleprajwal7-alt <gogawaleprajwal7@gmail.com>
2026-05-04 09:59:07 +0530

    Create ACTIVITY3.py

---
+++ exp2/ACTIVITY3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:57:53 2026
+
+@author: User
+"""
+
+salary = int(input("Enter your salary: "))
+
+if salary >= 25000:
+    print("Approved")
+else:
+    print("Rejected")
+    

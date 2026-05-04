9c72370bc6a55cdea7f2f2cea8742af64030539b
gogawaleprajwal7-alt <gogawaleprajwal7@gmail.com>
2026-05-04 09:46:09 +0530

    Create ACTIVITY2.py

---
+++ ACTIVITY2.py
@@ -0,0 +1,14 @@
+"""
+Created on Mon May  4 09:44:43 2026
+
+@author: User
+"""
+
+# Program to check movie ticket eligibility
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to enter.")
+else:
+    print("You are not eligible to enter.")

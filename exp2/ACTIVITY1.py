f0c8e585017b81d8c092648d23e7dfb02880c7de
gogawaleprajwal7-alt <gogawaleprajwal7@gmail.com>
2026-05-11 10:42:45 +0530

    Update ACTIVITY1.py

--- exp2/ACTIVITY1.py
+++ exp2/ACTIVITY1.py
@@ -1,7 +1,39 @@
-speed = int(input("Enter vehicle speed(km/h):"))
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 10:42:17 2026
 
-if speed > 60:
-      print("overspeeding!You have to pay a fine.")
+@author: Prajwal Gogawale
+"""
 
-else:
-  print("You are within the speed limit.")
+class MovieTicket:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Check eligibility
+    def check_eligibility(self):
+        if self.age >= 18:
+            return "Eligible for movie ticket 🎟️"
+        else:
+            return "Not eligible (Age must be 18 or above) ❌"
+
+    # Display details
+    def display(self):
+        print("\nMovie Ticket Details")
+        print(f"Name : {self.name}")
+        print(f"Age  : {self.age}")
+        print(self.check_eligibility())
+
+
+# Main program
+name = input("Enter your name: ")
+
+try:
+    age = int(input("Enter your age: "))
+
+    person = MovieTicket(name, age)
+    person.display()
+
+except ValueError:
+    print("Invalid input! Please enter a valid age.")
